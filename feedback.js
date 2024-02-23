ec98ae931476af8da87bc33a10bd8d34f61fd9e9
Jiwoong-Kang <jk990@byu.edu>
2024-02-22 19:47:40 -0700

    add value

--- feedback.js
+++ feedback.js
@@ -1,7 +1,7 @@
 function feedback(){
     const codeEl = document.querySelector("#message")
     
-    let string_code = JSON.stringify(codeEl)
+    let string_code = JSON.stringify(codeEl.value)
     localStorage.setItem("feedback", string_code)
     window.location.href = "sharing.html"
 
