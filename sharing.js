5a07657bdeaed4421a2c8423498d6e0513cc180a
Jiwoong-Kang <jk990@byu.edu>
2024-02-24 11:03:46 -0700

    javascript update

--- sharing.js
+++ sharing.js
@@ -1,3 +1,19 @@
 function feedback(){
     window.location.href = "feedback.html"
 }
+
+function sharing(){
+    let show_code = localStorage.getItem("code")
+    console.log(show_code)
+    let show_code_parse = JSON.parse(show_code)
+    document.getElementById("outer2_text").innerHTML = show_code_parse.code
+    document.getElementById("outer2_text").style.whiteSpace = "pre"
+    
+    document.getElementById("outer3_text").innerHTML = show_code_parse.explanation
+    document.getElementById("outer3_text").style.whiteSpace = "pre"
+
+    document.getElementById("subject2").innerHTML = show_code_parse.subject
+}
+
+
+sharing()
