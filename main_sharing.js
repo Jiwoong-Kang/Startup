86c2e08a82b38cb85a0526945e6f1b1aa9183c36
Jiwoong-Kang <jk990@byu.edu>
2024-02-25 19:15:29 -0700

    upload js

--- main_sharing.js
+++ main_sharing.js
@@ -5,6 +5,10 @@ function getting_title(){
     let title = localStorage.getItem("code")
     let show_code_parse = JSON.parse(show_code)
     document.getElementById("title").innerHTML = show_code_parse.subject
+    document.getElementById("title").addEventListener("click", function(){
+        // 클릭하면 원하는 페이지로 이동
+        window.location.href = "sharing.html";
+      });
     
 }
 
