f1884b6f6ebeaf2ec5df4e3d97cac3e33a523689
Jiwoong-Kang <jk990@byu.edu>
2024-02-25 19:13:54 -0700

    update main_sharing

--- main_sharing.js
+++ main_sharing.js
@@ -1,3 +1,11 @@
 function upload(){
     window.location.href = "code.html"
 }
+function getting_title(){
+    let title = localStorage.getItem("code")
+    let show_code_parse = JSON.parse(show_code)
+    document.getElementById("title").innerHTML = show_code_parse.subject
+    
+}
+
+getting_title()
