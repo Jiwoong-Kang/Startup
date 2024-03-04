c65aca7dced9645ffdddfd1c26d97c0dfee4e206
Jiwoong-Kang <jk990@byu.edu>
2024-03-04 16:08:10 -0700

    update index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
+
+app.use(express.json());
+
+app.use(express.static('public'));
+
+var apiRouter = express.Router();
+app.use(`/api`, apiRouter);
