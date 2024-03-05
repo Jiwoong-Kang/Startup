24a15aba8f05271a74b9426f86132a9d80805863
Jiwoong-Kang <jk990@byu.edu>
2024-03-04 19:37:19 -0700

    index.js update

--- index.js
+++ index.js
@@ -9,3 +9,11 @@ app.use(express.static('public'));
 
 var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
+
+app.use((_req, res) => {
+    res.sendFile('index.html', { root: 'public' });
+  });
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
+  
