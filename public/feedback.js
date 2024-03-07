c8a5a3f0a6c54db207e05b2280d070a5a37960fe
Jiwoong-Kang <jk990@byu.edu>
2024-03-06 17:03:06 -0700

    maybe???

--- public/feedback.js
+++ public/feedback.js
@@ -1,24 +1,24 @@
-async function sharing(){ 
-    
+function sharing(){  
     let show_code = localStorage.getItem("current_code")
     console.log(show_code)
     let show_code_parse = JSON.parse(show_code)
     document.getElementById("outer2_text").innerHTML = show_code_parse.code
     document.getElementById("outer2_text").style.whiteSpace = "pre"
-
 }
 
 
 async function feedback(){
-    let show_code = localStorage.getItem("current_code2")
+    let show_code = localStorage.getItem("current_code") //여기 2있었는데 일단 뺌
+    let show_code_parse = JSON.parse(show_code)
+    let real_code = show_code_parse.code
     try { 
-        const response1 = await fetch(`/api/getData?code=${show_code}`); 
+        const response1 = await fetch(`/api/getData?code=${real_code}`); 
         if (!response1.ok) {
             throw new Error(`HTTP error! status: ${response1.status}`);
         }
         const data = await response1.json();
         let feedback = document.getElementById("message").value
-        const response2 = await fetch(`/api/upDateFeedbacks?code=${show_code}`, { 
+        const response2 = await fetch(`/api/upDateFeedbacks?code=${real_code}`, { 
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
