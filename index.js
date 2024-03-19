54c9625aa72d1336d9a740bad1763b20a27144ab
Jiwoong-Kang <jk990@byu.edu>
2024-03-18 20:22:38 -0600

    login process(existed, new)

--- index.js
+++ index.js
@@ -52,6 +52,32 @@ apiRouter.post('/auth/create', async (req, res) => {
     res.status(204).end();
   });
 
+// GetUser returns information about a user
+apiRouter.get('/user/:email', async (req, res) => {
+    const user = await DB.getUser(req.params.email);
+    if (user) {
+      const token = req?.cookies.token;
+      res.send({ email: user.email, authenticated: token === user.token });
+      return;
+    }
+    res.status(404).send({ msg: 'Unknown' });
+  });
+  
+  // secureApiRouter verifies credentials for endpoints
+  var secureApiRouter = express.Router();
+  apiRouter.use(secureApiRouter);
+  
+  secureApiRouter.use(async (req, res, next) => {
+    authToken = req.cookies[authCookieName];
+    const user = await DB.getUserByToken(authToken);
+    if (user) {
+      next();
+    } else {
+      res.status(401).send({ msg: 'Unauthorized' });
+    }
+  });
+  
+
 let codes = []
 
 apiRouter.post('/save',(req, res) => {
