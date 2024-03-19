1a06be0a9b39cd90c17a579b394e0f668eac010c
Jiwoong-Kang <jk990@byu.edu>
2024-03-19 09:46:20 -0600

    put usercollection in db

--- database.js
+++ database.js
@@ -5,6 +5,8 @@ const config = require('./dbConfig.json');
 
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
+const db = client.db('startup');
+const userCollection = db.collention('user');
 
 function getUser(email) {
     return userCollection.findOne({ email: email });
