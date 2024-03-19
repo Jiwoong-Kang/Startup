92f51578ebb73ffc6f95ef41c000fa26ec5281b4
Jiwoong-Kang <jk990@byu.edu>
2024-03-18 20:28:05 -0600

    creating users

--- database.js
+++ database.js
@@ -5,3 +5,25 @@ const config = require('./dbConfig.json');
 
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
+
+function getUser(email) {
+    return userCollection.findOne({ email: email });
+}
+  
+function getUserByToken(token) {
+    return userCollection.findOne({ token: token });
+}
+  
+async function createUser(email, password) {
+    // Hash the password before we insert it into the database
+    const passwordHash = await bcrypt.hash(password, 10);
+  
+    const user = {
+      email: email,
+      password: passwordHash,
+      token: uuid.v4(),
+    };
+    await userCollection.insertOne(user);
+  
+    return user;
+}
