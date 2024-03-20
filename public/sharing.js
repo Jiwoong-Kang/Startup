f6fa15d0c04316807a04e468ec91d6a95b7922a5
Jiwoong-Kang <jk990@byu.edu>
2024-03-19 22:46:20 -0600

    tried to use db directly, but I am not sure

--- public/sharing.js
+++ public/sharing.js
@@ -2,10 +2,10 @@ function feedback2(){
     window.location.href = "feedback.html"
 }
 
-function sharing(){
+async function sharing(){
     let show_code = localStorage.getItem("current_code")
     let show_code_parse = JSON.parse(show_code)
-    
+        
     document.getElementById("subject2").innerHTML = show_code_parse.subject
     
     document.getElementById("new_outer2").innerHTML = show_code_parse.code
