f7e64d3f720667ed9e10742d02a3e2b16f676a3c
Jiwoong-Kang <jk990@byu.edu>
2024-03-26 00:46:14 -0600

    put websocket in code.js

--- public/code.js
+++ public/code.js
@@ -10,6 +10,12 @@ function getRandomFromTime() {
     return timeString;
 }
 
+const username = localStorage.getItem('userName'); // put username, new part
+const playerNameEl = document.querySelector('.player-name');
+playerNameEl.textContent = username;
+const FeedBackUpload = "Feedback uploaded";
+const CodeUpload = "Code uploaded";
+
 async function code(){ 
     const subjectEl = document.querySelector("#subject")
     const codeEl = document.querySelector("#message")
@@ -55,6 +61,7 @@ async function code(){
         if(!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
+        this.broadcastEvent(username, CodeUpload, show_code); // add event
         const data = await response.json();
         console.log('Success:', data);
         window.location.href = "main_sharing.html"
@@ -89,3 +96,37 @@ textAreas.forEach(textArea => {
         }
     });
 });
+
+function configureWebSocket(){ //애네들 function으로 써도 되는건가요?
+    const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
+    this.socket = new WebSocket(`${protocol}://${window.location.host}/ws`);
+    this.socket.onopen = (event) => {
+      this.displayMsg('system', 'server', 'connected'); // game -> server
+    };
+    this.socket.onclose = (event) => {
+      this.displayMsg('system', 'server', 'disconnected');
+    };
+    this.socket.onmessage = async (event) => {
+      const msg = JSON.parse(await event.data.text());
+      if (msg.type === FeedBackUpload) {
+        this.displayMsg('user', msg.from, `uploaded a feedback on ${msg.value.subject}`);
+      } else if (msg.type === CodeUpload) {
+        this.displayMsg('user', msg.from, `uploaded a new code`); //player -> user
+      }
+    };
+}
+
+function displayMsg(cls, from, msg) { // need to move to mypage, mainsharing, sharing
+    const chatText = document.querySelector('#user-messages'); 
+    chatText.innerHTML =
+      `<div class="event"><span class="${cls}-event">${from}</span> ${msg}</div>` + chatText.innerHTML;
+  }
+
+function broadcastEvent(from, type, value) {
+    const event = {
+      from: from,
+      type: type,
+      value: value,
+    };
+    this.socket.send(JSON.stringify(event)); // this 대신에 이 정보를 socket에 저장할 수 있는 방법을 찾고 그 socket을 불러와야 한다.
+  }
