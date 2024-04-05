0e1107dc24bcee8561df05a153b7b752f5959ed8
Jiwoong-Kang <jk990@byu.edu>
2024-04-05 00:38:18 -0600

    create value account

--- src/account/account.jsx
+++ src/account/account.jsx
@@ -1,6 +1,32 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
 
 export function Account() {
+    const [userName, setUserName] = React.useState('');
+    const [password, setPassword] = React.useState('');
+
+    async function createUser() {
+        loginOrCreate(`/api/auth/create`);
+      }
+    
+      async function loginOrCreate(endpoint) {
+        const response = await fetch(endpoint, {
+          method: 'post',
+          body: JSON.stringify({email: userName, password: password}),
+          headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+          },
+        });
+        if (response?.status === 200) {
+          localStorage.setItem('userName', userName);
+          props.onLogin(userName);
+        } else {
+          const body = await response.json();
+          setDisplayError(`⚠ Error: ${body.msg}`);
+        }
+      }
+
     return (
         <main className='container-fluid bg-secondary text-center'>
             <p>Put your new ID and new Password</p>
