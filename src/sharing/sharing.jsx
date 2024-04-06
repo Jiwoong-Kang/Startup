48247449b7993ae1c2ae6406dfb1c6a08190ffd3
Jiwoong-Kang <jk990@byu.edu>
2024-04-06 00:29:26 -0600

    basic format of sharing

---
+++ src/sharing/sharing.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export function Sharing(){
+    return(
+        <main>
+      <div class="users">
+        User
+        <span class="user-name"></span>
+        <div id="user-messages"></div>
+      </div>
+      <h2 id = "subject2">subject that you entered</h2>
+      <div id="new_outer">
+        <div id="new_outer2">show code
+        </div>
+        <div id="new_outer3">show explanation
+        </div>
+      </div>
+      <h3>Feedbacks</h3>
+      <div id="user-messages">
+        <div id="feedback2">feedbacks</div>
+      </div>
+      <div id="post_form">
+        <button type="submit" id="feedback" onclick = "feedback2()">Leave your feedback</button>
+      </div>
+      <div><button type="submit" id="delete" onclick = "deleting()">Delete code</button></div>
+      
+    </main>
+    )
+}
