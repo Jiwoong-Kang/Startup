0df55bc835de4ff09f882b954499d292ed63cfb5
Jiwoong-Kang <jk990@byu.edu>
2024-04-07 23:29:39 -0600

    fixing error message

--- src/login/errorpopup.jsx
+++ src/login/errorpopup.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 export function ErrorPopup({ message, onHide }) {
+
   return (
     <div id="errorPopup" className="error-popup">
       <span className="close-btn" onClick={onHide}>X</span>
