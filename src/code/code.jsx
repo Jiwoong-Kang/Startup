3ded053327e5199a0a4f14910207d1c54f0671e7
Jiwoong-Kang <jk990@byu.edu>
2024-04-07 21:39:40 -0600

    made websocket file

--- src/code/code.jsx
+++ src/code/code.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Events, Notifier} from '../websocket';
+import { Events, Notifier} from '../websocket/websocket';
 
 
 function getRandomFromTime() {
--- src/feedback/feedback.jsx
+++ src/feedback/feedback.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Events, Notifier} from "../websocket";
+import { Events, Notifier} from "../websocket/websocket";
 
 export function Feedback() {
     const [currentCode, setCurrentCode] = useState({ code: "Show the code that someone uploaded" });
